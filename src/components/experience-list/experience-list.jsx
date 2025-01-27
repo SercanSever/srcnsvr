fa0fb66faa2761168a935460e69cad21d583dc6b
SercanSever <sercan.sever16@gmail.com>
2025-01-27 18:43:23 +0300

    refactor experience section; extract experience items into a new component and update styling

---
+++ src/components/experience-list/experience-list.jsx
@@ -0,0 +1,92 @@
+import "./experience-list.css";
+
+const ExperienceList = () => {
+  return (
+    <>
+      <div className="content">
+        <div className="timeLine">
+          <div className="dot"></div>
+        </div>
+        <div className="experienceItem">
+          <div className="text">
+            <p>
+              Worked as a Backend Developer <span>at</span> Company.
+            </p>
+            <p className="info">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+              hic facere libero repellendus incidunt, nostrum et, possimus
+              distinctio iste vel maiores? Magnam dolore ullam hic ex excepturi
+              saepe cum sint?
+            </p>
+          </div>
+          <span className="year">2024 - Present</span>
+        </div>
+      </div>
+      <div className="content">
+        <div className="timeLine">
+          <div className="dot"></div>
+          <div className="line"></div>
+        </div>
+        <div className="experienceItem">
+          <div className="text">
+            <p>
+              Worked as a Backend Developer <span>at</span> Company.
+            </p>
+            <p className="info">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+              hic facere libero repellendus incidunt, nostrum et, possimus
+              distinctio iste vel maiores? Magnam dolore ullam hic ex excepturi
+              saepe cum sint? Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Numquam hic facere libero repellendus incidunt,
+              nostrum et, possimus distinctio iste vel maiores? Magnam dolore
+              ullam hic ex excepturi saepe cum sint?
+            </p>
+          </div>
+          <span className="year">2024 - 2025</span>
+        </div>
+      </div>
+      <div className="content">
+        <div className="timeLine">
+          <div className="dot"></div>
+          <div className="line"></div>
+        </div>
+        <div className="experienceItem">
+          <div className="text">
+            <p>
+              Worked as a Backend Developer <span>at</span> Company.
+            </p>
+            <p className="info">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+              hic facere libero repellendus incidunt, nostrum et, possimus
+              distinctio iste vel maiores? Magnam dolore ullam hic ex excepturi
+              saepe cum sint?
+            </p>
+          </div>
+          <span className="year">2024 - 2025</span>
+        </div>
+      </div>
+      <div className="content">
+        <div className="timeLine">
+          <div className="dot"></div>
+          <div className="line"></div>
+        </div>
+        <div className="experienceItem">
+          <div className="text">
+            <p>
+              Worked as a Backend Developer <span>at</span> Company.
+            </p>
+            <p className="info">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+              hic facere libero repellendus incidunt, nostrum et, possimus
+              distinctio iste vel maiores? Magnam dolore ullam hic ex excepturi
+              saepe cum sint?
+            </p>
+          </div>
+          <span className="year">2024 - 2025</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExperienceList;
--- src/pages/home/experience/experience.jsx
+++ src/pages/home/experience/experience.jsx
@@ -1,3 +1,4 @@
+import ExperienceList from "../../../components/experience-list/experience-list";
 import "./experience.css";
 const Experience = () => {
   return (
@@ -5,88 +6,7 @@ const Experience = () => {
       <h1>Experience</h1>
       <div className="experienceContainer">
         <div className="line"></div>
-        <div className="content">
-          <div className="timeLine">
-            <div className="dot"></div>
-          </div>
-          <div className="experienceItem">
-            <div className="text">
-              <p>
-                Worked as a Backend Developer <span>at</span> Company.
-              </p>
-              <p className="info">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Numquam hic facere libero repellendus incidunt, nostrum et,
-                possimus distinctio iste vel maiores? Magnam dolore ullam hic ex
-                excepturi saepe cum sint?
-              </p>
-            </div>
-            <span className="year">2024 - Present</span>
-          </div>
-        </div>
-        <div className="content">
-          <div className="timeLine">
-            <div className="dot"></div>
-            <div className="line"></div>
-          </div>
-          <div className="experienceItem">
-            <div className="text">
-              <p>
-                Worked as a Backend Developer <span>at</span> Company.
-              </p>
-              <p className="info">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Numquam hic facere libero repellendus incidunt, nostrum et,
-                possimus distinctio iste vel maiores? Magnam dolore ullam hic ex
-                excepturi saepe cum sint? Lorem ipsum, dolor sit amet
-                consectetur adipisicing elit. Numquam hic facere libero
-                repellendus incidunt, nostrum et, possimus distinctio iste vel
-                maiores? Magnam dolore ullam hic ex excepturi saepe cum sint?
-              </p>
-            </div>
-            <span className="year">2024 - 2025</span>
-          </div>
-        </div>
-        <div className="content">
-          <div className="timeLine">
-            <div className="dot"></div>
-            <div className="line"></div>
-          </div>
-          <div className="experienceItem">
-            <div className="text">
-              <p>
-                Worked as a Backend Developer <span>at</span> Company.
-              </p>
-              <p className="info">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Numquam hic facere libero repellendus incidunt, nostrum et,
-                possimus distinctio iste vel maiores? Magnam dolore ullam hic ex
-                excepturi saepe cum sint?
-              </p>
-            </div>
-            <span className="year">2024 - 2025</span>
-          </div>
-        </div>
-        <div className="content">
-          <div className="timeLine">
-            <div className="dot"></div>
-            <div className="line"></div>
-          </div>
-          <div className="experienceItem">
-            <div className="text">
-              <p>
-                Worked as a Backend Developer <span>at</span> Company.
-              </p>
-              <p className="info">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Numquam hic facere libero repellendus incidunt, nostrum et,
-                possimus distinctio iste vel maiores? Magnam dolore ullam hic ex
-                excepturi saepe cum sint?
-              </p>
-            </div>
-            <span className="year">2024 - 2025</span>
-          </div>
-        </div>
+        <ExperienceList />
       </div>
     </div>
   );
